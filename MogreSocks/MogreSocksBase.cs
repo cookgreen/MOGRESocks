30517d97b9654eeec5a23249e8765b9a2d3e7a7e
DoDoCat <dell20091234@gmail.com>
2022-11-21 16:32:45 +0800

    MogreSocksConnData and MogreSocksLogger

--- MogreSocks/MogreSocksBase.cs
+++ MogreSocks/MogreSocksBase.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace MogreSocks
 {
+    public struct ThreadRecvData
+    {
+        public byte[] Data { get; set; }
+        public string IPAddr { get; set; }
+        public ulong Port { get; set; }
+    }
+
     public class MogreSocksBase
     {
+        public event Action<string, ulong, int> Connect;
+        public event Action<byte[], string, ulong> Receive;
+        public event Action<string, ulong> Disconnect;
     }
 }
